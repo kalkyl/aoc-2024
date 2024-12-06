b246205afe436e24d6d1e10c0cdf6e534aa0fc8f
kalkyl <henrik.alser@me.com>
2024-12-06 14:09:44 +0100

    Day 6

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -21,23 +21,23 @@ fn main() {
 }
 
 fn run(mut grid: Vec<Vec<char>>) -> Option<usize> {
-    let mut trace: std::collections::HashSet<((usize, usize), (isize, isize))> = Default::default();
+    let mut trace: std::collections::HashSet<((isize, isize), (isize, isize))> = Default::default();
     let (mut x, mut y) = grid
         .iter()
         .enumerate()
-        .find_map(|(y, r)| r.iter().position(|c| *c == '^').map(|x| (x, y)))
+        .find_map(|(y, r)| r.iter().position(|c| *c == '^').map(|x| (x as isize, y as isize)))
         .unwrap();
     let (w, h) = (grid[0].len() as isize, grid.len() as isize);
     let mut d = [-1, 0, 1, 0];
 
-    grid[y][x] = 'X';
-    while (0..h).contains(&(y as isize + d[0])) && (0..w).contains(&(x as isize + d[1])) {
-        while grid[(y as isize + d[0]) as usize][(x as isize + d[1]) as usize] == '#' {
+    grid[y as usize][x as usize] = 'X';
+    while (0..h).contains(&(y + d[0])) && (0..w).contains(&(x + d[1])) {
+        while grid[(y + d[0]) as usize][(x + d[1]) as usize] == '#' {
             d.rotate_left(1);
         }
-        y = (y as isize + d[0]) as usize;
-        x = (x as isize + d[1]) as usize;
-        grid[y][x] = 'X';
+        y += d[0];
+        x += d[1];
+        grid[y as usize][x as usize] = 'X';
         if !trace.insert(((x, y), (d[1], d[0]))) {
             return None;
         }
