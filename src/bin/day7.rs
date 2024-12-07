34cf793c294846787df9b997f7915cf4ad1eec83
kalkyl <henrik.alser@me.com>
2024-12-07 12:47:34 +0100

    Day 7

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -22,11 +22,11 @@ fn sum_valid(equations: &[(u64, Vec<u64>)], operators: &[fn(u64, u64) -> u64]) -
     equations
         .iter()
         .filter(|(value, numbers)| {
-            (0..operators.len().pow((numbers.len() - 1) as u32)).any(|combination| {
-                let combination_ops = to_base(operators.len(), combination);
+            (0..operators.len().pow((numbers.len() - 1) as u32)).any(|variant| {
+                let op_pattern = to_base(operators.len(), variant);
                 let mut total = numbers[0];
                 for i in 0..numbers.len() - 1 {
-                    let op = *combination_ops.get(i).unwrap_or(&0);
+                    let op = *op_pattern.get(i).unwrap_or(&0);
                     total = operators[op](total, numbers[i + 1]);
                 }
                 total == *value
